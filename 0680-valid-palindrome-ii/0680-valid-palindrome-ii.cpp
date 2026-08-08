a5371b1610c8e5fc5479c27f45d066c59ff18e63
siddiquio786anas-crypto <siddiquio786anas@gmail.com>
2026-08-09 00:29:18 +0530

    Time: 7 ms (28.94%), Space: 27.5 MB (5.94%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkPalindrome(string s,int i,int j){
+        while(i<=j){
+            if(s[i]!=s[j]){
+            return false;
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+    };
+    bool validPalindrome(string s) {
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j){
+            if(s[i]==s[j]){
+                i++;
+                j--;
+            }
+            else{
+                bool c1=checkPalindrome(s,i,j-1);
+                bool c2=checkPalindrome(s,i+1,j);
+                bool fr=c1||c2;
+                return fr;
+            }
+        }
+        return true;
+
+    }
+};
